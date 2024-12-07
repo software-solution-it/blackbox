919a93ca034062e2bb302c71a5ec1c6dbdfe53f0
Seu Nome <seuemail@dominio.com>
2024-12-07 14:10:56 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './routes/AppRoutes';
 import './index.css';
 import './styles/global.css';
 
+// Adicione o script do Google Tag Manager
+(function () {
+  window.dataLayer = window.dataLayer || [];
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=AW-16813181591";
+  document.head.appendChild(script);
+
+  script.onload = () => {
+    function gtag() {
+      // eslint-disable-next-line no-undef
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'AW-16813181591');
+  };
+})();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppRoutes />,
+    <AppRoutes />
   </React.StrictMode>
 );
